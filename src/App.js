9e74b2a986171b9ef97f976fd7c16091fe5e14ad
dallas <dallas.wty@gmail.com>
2023-02-06 15:57:47 +0900

    Firebase Get

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import db from './firebase';
-import { collection, getDocs } from 'firebase/firestore';
-import { useEffect } from 'react';
+import { collection, doc, getDocs } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
 
 function App() {
-
+  const [todos, setList] = useState([]);
   const todoCollectionRef = collection(db, "todos");
 
-  useEffect(() => {
-    const getList = async() => {
+  useEffect( () => {
+    const getList = async () => {
       const data = await getDocs(todoCollectionRef);
-      console.log(data);
+      // data.docs.map( (doc) => setList([...todos, doc]) )
+      setList(data.docs.map(
+        (doc) => {return ({ ...doc.data(), id:doc.id })}
+        )
+      )
     }
-
     getList();
   },[])
+
+  const showList = todos.map(
+    (value) => {return (
+      <div key={value.id}>
+        <h2>{value.content}</h2>
+      </div>
+    )}
+  )
 
   return (
     <div className="App">
       <h1>Firebase, React</h1>
+      {showList}
     </div>
   );
 }
