9960b97192a73994808a0ce8a66c0e81c10a60dd
dallas <dallas.wty@gmail.com>
2023-02-06 15:20:43 +0900

    Firestore

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
+import db from './firebase';
+import { collection, getDocs } from 'firebase/firestore';
+import { useEffect } from 'react';
+
 function App() {
+
+  const todoCollectionRef = collection(db, "todos");
+
+  useEffect(() => {
+    const getList = async() => {
+      const data = await getDocs(todoCollectionRef);
+      console.log(data);
+    }
+
+    getList();
+  },[])
+
   return (
     <div className="App">
       <h1>Firebase, React</h1>
