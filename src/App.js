01560e34dd01d7cbed11e92f38af7db8fa0c0f2b
dallas <dallas.wty@gmail.com>
2023-02-06 16:32:17 +0900

    Add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import db from './firebase';
-import { collection, doc, getDocs } from 'firebase/firestore';
+import { collection, doc, getDocs, addDoc } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 
 function App() {
   const [todos, setList] = useState([]);
   const todoCollectionRef = collection(db, "todos");
+  const [newData, setNewData] = useState('');
 
   useEffect( () => {
     const getList = async () => {
@@ -22,13 +23,27 @@ function App() {
     (value) => {return (
       <div key={value.id}>
         <h2>{value.content}</h2>
+        <p>{value.id}</p>
       </div>
     )}
   )
 
+  const date = new Date();
+  const now_date = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate();
+
+  const createList = async () => {
+    // await addDoc(연결객체, 전달 값)
+    await addDoc(todoCollectionRef, {content:newData, d_date:now_date})
+    document.querySelector('#todoValue').value = '';
+  }
+
   return (
     <div className="App">
       <h1>Firebase, React</h1>
+      <input id='todoValue' type='text' placeholder='todos...' onChange={(e) => {setNewData(e.target.value);}} />
+      <button onClick={createList}>Add List</button>
+      <p>{newData}</p>
+      <hr />
       {showList}
     </div>
   );
